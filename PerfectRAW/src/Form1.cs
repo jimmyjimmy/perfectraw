9bf406ff3dc3356b0c0a948e3e0e97cf61fae5a3
aleixventa <aleixventa@794c2a0b-cc4c-0410-8f29-2f7fc8aad77f>
2008-05-11 13:40:02 +0000

    Directory name is missing an "r"

--- PerfectRAW/src/Form1.cs
+++ PerfectRAW/src/Form1.cs
@@ -22,7 +22,9 @@ namespace DCRAwGUI
         private TextBox textBox1;
         private Label label2;
         private OpenFileDialog openFileDialog1;
-        private Button button2;        
+        private Button button2;
+        private Button button3;
+        private Button button4;        
 
 		/// <summary>
 		/// Variable del diseñador requerida.
@@ -71,11 +73,14 @@ namespace DCRAwGUI
             this.label2 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(12, 441);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(85, 24);
@@ -85,16 +90,19 @@ namespace DCRAwGUI
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(12, 34);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(570, 401);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(103, 447);
             this.label1.Name = "label1";
@@ -104,6 +112,7 @@ namespace DCRAwGUI
             // 
             // camera
             // 
+            this.camera.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.camera.AutoSize = true;
             this.camera.Location = new System.Drawing.Point(155, 447);
             this.camera.Name = "camera";
@@ -141,10 +150,34 @@ namespace DCRAwGUI
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(508, 442);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(427, 442);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
             this.ClientSize = new System.Drawing.Size(595, 477);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -202,6 +235,19 @@ namespace DCRAwGUI
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox1.Text = @"c:\dcraw\raws\1.cr2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.SizeMode == PictureBoxSizeMode.Normal)
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("aaa.jpg", ImageFormat.Tiff);
         }
 	}
 
